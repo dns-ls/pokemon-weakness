aa6843d67e1182929b313ae7e16b0a31cb6da61f
denis <34921068+dns-ls@users.noreply.github.com>
2025-11-07 11:27:52 +0100

    hud funktioniert nun

--- hud.py
+++ hud.py
@@ -1,18 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
+import time
 
 def update_labels(x4: list, x2: list, x05: list, x025: list, x0: list):
-	global types_4, types_2, types_0_5, types_0_25, types_0
+	global types_4, types_2, types_0_5, types_0_25, types_0, root
 	types_4.config(text=str(x4))
 	types_2.config(text=str(x2))
 	types_0_5.config(text=str(x05))
 	types_0_25.config(text=str(x025))
 	types_0.config(text=str(x0))
-	root.after(10000, update_labels)
+	root.update()
 
 root = tk.Tk()
-frm = ttk.Frame(root, padding=10)
+frm = ttk.Frame(root, padding=10, )
 frm.grid()
+
+def exit_button_func():
+	global root
+	root.mainloop()
+	root.destroy()
 
 label_4 = ttk.Label(frm, text="4x", justify="left")
 label_4.grid(row=0, column=0)
@@ -39,6 +45,10 @@ label_0.grid(row=4, column=0)
 types_0 = ttk.Label(frm, text="", justify="left")
 types_0.grid(row=4, column=1)
 
-update_labels()
-root.mainloop()
+root.minsize(280, 200)
 
+root.update()
+
+# exit_button = ttk.Button(frm, text="Exit", command=exit_button_func)
+# exit_button.grid(row=5, column=0, columnspan=2)
+# not working under current approach
